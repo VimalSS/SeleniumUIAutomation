71917ff81aa44d60eb891fc33fa05b729057c41d
vimalrepo <vimaljob@yahoo.com>
2019-07-07 19:28:21 -0500

    New comment added..

--- src/POM/BestBuyCheckOutPage.java
+++ src/POM/BestBuyCheckOutPage.java
@@ -9,10 +9,11 @@ public class BestBuyCheckOutPage {
 	public static WebElement element = null;
 
 	
-
+// This is for checkout
 	public static WebElement btnCheckOut(WebDriver driver) {
 		element = driver.findElement(By.xpath("*//button[contains(text(), 'Checkout')]"));
 		return element;
+		//this is to checkout
 	}
 
 	
